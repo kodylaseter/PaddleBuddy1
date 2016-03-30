24f6a7284b5f8cb86529aa7677c09a8300987a3e
Kody Laseter <kodylaseter1@gmail.com>
2016-03-30 17:45:22 -0400

    work on map fragment

--- PaddleBuddy/PaddleBuddy.Droid/Fragments/MapFragment.cs
+++ PaddleBuddy/PaddleBuddy.Droid/Fragments/MapFragment.cs
@@ -1,6 +1,8 @@
-﻿using Android.Gms.Maps;
+﻿
+using Android.Gms.Maps;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V4.App;
 using Android.Views;
 using Android.Widget;
 using MvvmCross.Binding.Droid.BindingContext;
@@ -14,8 +16,6 @@ namespace PaddleBuddy.Droid.Fragments
     public class MapFragment : BaseFragment<MapViewModel>
     {
         protected override int FragmentId => Resource.Layout.fragment_map;
-        //private GoogleMap _map;
-        //private FrameLayout _map;
 
         //public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         //{
@@ -23,9 +23,20 @@ namespace PaddleBuddy.Droid.Fragments
 
         //    var view = this.BindingInflate(Resource.Layout.fragment_map, null);
 
-        //    _map = view.FindViewById<FrameLayout>(Resource.Id.map);
-
-
+        //    if (view != null)
+        //    {
+        //        ViewGroup parent = (ViewGroup)view.getParent();
+        //        if (parent != null)
+        //            parent.removeView(view);
+        //    }
+        //    try
+        //    {
+        //        view = inflater.inflate(R.layout.map, container, false);
+        //    }
+        //    catch (InflateException e)
+        //    {
+        //        /* map is already there, just return view as it is */
+        //    }
 
         //    return View;
         //}
