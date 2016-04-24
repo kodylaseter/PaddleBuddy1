e321a127c14a4eaa2338fb16a4d028f44e92cb1c
Kody Laseter <kodylaseter1@gmail.com>
2016-04-24 11:24:19 -0400

    working search

---
+++ PaddleBuddy/PaddleBuddy.Core/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Platform;
+using MvvmCross.Plugins.Messenger;
+
+namespace PaddleBuddy.Core.Services
+{
+    public class BaseService
+    {
+        public IMvxMessenger Messenger { get; set; }
+
+        protected BaseService()
+        {
+            Messenger = Mvx.Resolve<IMvxMessenger>();
+        }
+    }
+}
